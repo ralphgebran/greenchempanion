5110024c60d6af03016c1f2552446fb9b2751604
ralphgebran <ralphgebran41@gmail.com>
2025-05-06 13:37:46 +0200

    Imported functions in __init__.py and removed gcp from env_win.yml

--- src/greenchempanion/__init__.py
+++ src/greenchempanion/__init__.py
@@ -1,5 +1,6 @@
 """Your interactive companion for Green Chemistry."""
 
 from __future__ import annotations
+from .functions import Atom_Count_With_H, Reaction, compute_PMI, canonicalize_smiles, compute_E
 
 __version__ = "0.0.1"
