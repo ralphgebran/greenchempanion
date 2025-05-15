2ab0debea0aadde860ab4380a97178c41a7a7b26
ralphgebran <ralphgebran41@gmail.com>
2025-05-15 18:08:30 +0200

    Imported more functions in init file

--- src/greenchempanion/__init__.py
+++ src/greenchempanion/__init__.py
@@ -2,5 +2,6 @@
 
 from __future__ import annotations
 from .functions import Atom_Count_With_H, Reaction, compute_PMI, canonicalize_smiles, compute_E
+from .functions import get_solvent_info, waste_efficiency, PMI_assesment, Atom_ec_assesment, logP_assessment_molecule, atoms_assessment, structural_assessment
 
 __version__ = "0.0.1"
